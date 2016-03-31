bedcc669fb19eece975c96d9156a0815d366175d
VicChan <victorchanchina@gmail.com>
2016-03-31 14:33:41 +0800

    Initial Commit

---
+++ HelloNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloNote
+//
+//  Created by VicChan on 3/31/16.
+//  Copyright © 2016 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloNote
+//
+//  Created by VicChan on 3/31/16.
+//  Copyright © 2016 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
